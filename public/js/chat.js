404e26b52bd7e75232944cb9bb83b346cc5882e8
Nicolás San Pedro <nicolassanpedro@Nicolass-MacBook-Pro.local>
2020-05-24 01:39:28 -0300

    disabling btns

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,48 @@
+'use strict';
 const socket = io();
+
+const messageForm = document.querySelector('#message-form');
+const formInput = messageForm.querySelector('input');
+const formBtn = messageForm.querySelector('button');
+const sendLocation = document.querySelector('#send-location');
 
 socket.on('message', (message) => {
     console.log(message);
 });
 
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    messageForm.setAttribute('disabled', 'disabled');
+    
+
     const msg = e.target.elements.message.value;
+    
     socket.emit('sendMessage', msg, (err) => {
         if (err) return console.log('err ==> ', err);
+
+        messageForm.removeAttribute('disabled');
+        formInput.value = '';
+        formInput.focus();
 
         console.log('Message delivered!');
     });
 })
 
 
-document.querySelector('#send-location').addEventListener('click', () => {
+sendLocation.addEventListener('click', () => {
     if (!navigator.geolocation) return alert('Geolocation not supported by browser');
+
+    sendLocation.setAttribute('disabled', 'disabled');
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
-            console.log('Location Shared');
-            
+            sendLocation.removeAttribute('disabled');
+                console.log('Location Shared');
         });
 
     });
